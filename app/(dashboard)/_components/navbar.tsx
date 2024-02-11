51f97d1e5c91683e32843bb9f26650f17659dc16
Ron Almog <ron8132@gmail.com>
2024-02-10 19:09:15 -0500

    add search with useDebounceValue

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
+import SearchInput from "./search-input";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-200">
-      <div className="hidden lg:flex lg:flex-1">search</div>
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
       <UserButton />
     </div>
   );
