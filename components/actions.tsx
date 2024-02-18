fb354ac4d8bb51535fa3f5ce30a3bd45b3b35552
Ron Almog <ron8132@gmail.com>
2024-02-18 17:57:36 -0500

    open and close modal with zustand

--- components/actions.tsx
+++ components/actions.tsx
@@ -7,12 +7,13 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Link2, Trash2 } from "lucide-react";
+import { Link2, Pencil, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
 import ConfirmModal from "./confirm-modal";
 import { Button } from "./ui/button";
+import { useRenameModal } from "@/store/use-rename-modal";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -30,7 +31,7 @@ export const Actions = ({
   title,
 }: ActionsProps) => {
   const { mutate, pending } = useApiMutation(api.board.remove);
-
+  const { onOpen } = useRenameModal();
   const onDelete = () => {
     mutate({ id })
       .then(() => toast.success("Board deleted"))
@@ -55,6 +56,13 @@ export const Actions = ({
         <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
           <Link2 className="h-4 w-4 mr-2" />
           Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="p-3 cursor-pointer"
+          onClick={() => onOpen(id, title)}
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
         </DropdownMenuItem>
         <ConfirmModal
           header="Delete board?"
