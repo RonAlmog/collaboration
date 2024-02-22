93c61fe5374f1bf405abaf9ea60d355f4f771ee7
Ron Almog <ron8132@gmail.com>
2024-02-21 20:39:30 -0500

    filter for favorite boards

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -6,8 +6,10 @@ import { api } from "@/convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const EmptyBaords = () => {
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -16,6 +18,7 @@ export const EmptyBaords = () => {
     mutate({ title: "Untitled", orgId: organization.id })
       .then((id) => {
         toast.success("Board created!");
+        router.push(`/board/${id}`);
       })
       .catch((error) => {
         toast.error("Failed to create board");
