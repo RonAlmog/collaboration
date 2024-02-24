c483bcae800fa636a570b481375994cc40db474d
Ron Almog <ron8132@gmail.com>
2024-02-24 11:37:58 -0500

    add participants and user avatar

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,32 @@
-type Props = {};
+"use client";
 
-const Participants = (props: Props) => {
+import { useOthers, useSelf } from "@/liveblocks.config";
+import UserAvatar from "./user-avatar";
+
+const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const MAX_SHAWN_USERS = 2;
+  const hasMoreUsers = users.length > MAX_SHAWN_USERS;
   return (
     <div className="absolute top-2 right-2 bg-white rounded-md px-3 h-12 flex items-center shadow-md">
-      list of users
+      {users.slice(0, MAX_SHAWN_USERS).map(({ connectionId, info }) => {
+        return (
+          <UserAvatar
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name?.[0] || "T"}
+          />
+        );
+      })}
+      {currentUser && (
+        <UserAvatar
+          src={currentUser.info?.picture}
+          name={`${currentUser.info?.name} (You)`}
+          fallback={currentUser.info?.name?.[0]}
+        />
+      )}
     </div>
   );
 };
