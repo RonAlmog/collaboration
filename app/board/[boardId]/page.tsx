ac9043ed630ed818a82ae48e3c65181d7ab74d82
Ron Almog <ron8132@gmail.com>
2024-02-24 09:27:06 -0500

    add liveblocks auth

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Canvas from "./_components/canvas";
 import { Room } from "@/components/room";
 import CanvasLoading from "./_components/canvas-loading";
+// import { useSelf } from "@/liveblocks.config";
 
 interface BoardPageProps {
   params: { boardId: string };
 }
 
 const BoardPage = ({ params }: BoardPageProps) => {
-  return <CanvasLoading />;
+  // const { name, picture } = useSelf((me) => me.info);
   return (
     <Room roomId={params.boardId} fallback={<CanvasLoading />}>
       <Canvas boardId={params.boardId} />
